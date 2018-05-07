d87d4183abdf7e520f4d406c8079195642215e44
Allison <allison.cook@ocsbstudent.ca>
2018-05-07 12:43:50 -0400

    Initial commit

---
+++ ArrayAverageAllisonC/ArrayAverageForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ArrayAverageAllisonC
+{
+    partial class frmArrayAverage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.btnCalculateAverage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(97, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(91, 33);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(164, 220);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(109, 21);
+            this.lblAverage.TabIndex = 1;
+            this.lblAverage.Text = "The average is";
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(83, 51);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(120, 147);
+            this.lstNumbers.TabIndex = 2;
+            // 
+            // btnCalculateAverage
+            // 
+            this.btnCalculateAverage.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculateAverage.Location = new System.Drawing.Point(12, 214);
+            this.btnCalculateAverage.Name = "btnCalculateAverage";
+            this.btnCalculateAverage.Size = new System.Drawing.Size(146, 33);
+            this.btnCalculateAverage.TabIndex = 3;
+            this.btnCalculateAverage.Text = "Calculate Average";
+            this.btnCalculateAverage.UseVisualStyleBackColor = true;
+            this.btnCalculateAverage.Click += new System.EventHandler(this.btnCalculateAverage_Click);
+            // 
+            // frmArrayAverage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 261);
+            this.Controls.Add(this.btnCalculateAverage);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmArrayAverage";
+            this.Text = "Array Average by AllisonC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Button btnCalculateAverage;
+    }
+}
+
---
+++ ArrayAverageAllisonC/ArrayAverageForm.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Created by: Allison Cook
+ * Created on: May the 4th, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #34 - Introduction to Arrays
+ * This program display 10 randomly generated numbers and calculates their average
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayAverageAllisonC
+{
+    public partial class frmArrayAverage : Form
+    {
+        //constant for array lenght
+        const int ARRAY_SIZE = 10;
+        //making the array
+        int[] arrayOfNumbers = new int[ARRAY_SIZE];
+
+        public frmArrayAverage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            int randomNumber;
+            int counter;
+            Random randomNumberGenerator = new Random();
+
+            //clearing the list box
+            lstNumbers.Items.Clear();
+
+            //creating random numbers and adding them to the array
+            for (counter = 0; counter < ARRAY_SIZE; counter++)
+            {
+                //generate the random number
+                randomNumber = randomNumberGenerator.Next(1, ARRAY_SIZE);
+
+                //assign the random number to the array at the index of counter
+                arrayOfNumbers[counter] = randomNumber;
+
+                //add the random number to the list box
+                this.lstNumbers.Items.Add(randomNumber);
+
+                //refresh the form to show the updated list box
+                this.Refresh();
+            }
+        }
+
+        private void btnCalculateAverage_Click(object sender, EventArgs e)
+        {
+            //decalre local variables
+            double average = 0, sum = 0;
+            int counter;
+
+            //calculate the sum of all the numbers in the array
+            for (counter = 0; counter < arrayOfNumbers.Length; counter++)
+            { 
+                //adding the number together
+                sum = sum + arrayOfNumbers[counter];
+            }
+
+            //calculate the average
+            average = sum / arrayOfNumbers.Length;
+
+            //display the average
+            this.lblAverage.Text = "The average is " + average;
+        }
+    }
+}
